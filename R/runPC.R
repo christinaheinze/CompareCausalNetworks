77d96431098da7bbc2c73291425dc34f907f4ccc
Takeshi Teshima <takeshi.diadochos@gmail.com>
2016-01-28 22:23:18 +0900

    [#1] Use variableSelMat for fixedEdges of PC algorithm

--- R/runPC.R
+++ R/runPC.R
@@ -2,7 +2,7 @@ runPC <- function(X, parentsOf, alpha, variableSelMat, setOptions, directed, ver
                    result){
   
   # additional options for PC
-  optionsList <- list("indepTest"=pcalg::gaussCItest, "fixedEdges"=NULL,
+  optionsList <- list("indepTest"=pcalg::gaussCItest,
                       "NAdelete"=TRUE, "m.max"=Inf, "u2pd", 
                       "skel.method"= "stable", "conservative"=FALSE,
                       "maj.rule"=FALSE, "solve.confl"=FALSE)
@@ -14,8 +14,8 @@ runPC <- function(X, parentsOf, alpha, variableSelMat, setOptions, directed, ver
   suffStat <- list(C = cor(X), n = nrow(X))
   pc.fit <- pcalg::pc(suffStat, indepTest = optionsList$indepTest, p=ncol(X), 
                alpha = alpha, 
-               fixedGaps= if(is.null(variableSelMat)) NULL else (!variableSelMat), 
-               fixedEdges = optionsList$fixedEdges, 
+               fixedGaps= ifelse(is.null(variableSelMat), NULL, (!variableSelMat)),
+               fixedEdges = ifelse(is.null(variableSelMat), NULL, (variableSelMat)),
                NAdelete= optionsList$NAdelete, m.max= optionsList$m.max, 
                u2pd=optionsList$u2pd, skel.method= optionsList$skel.method, 
                conservative= optionsList$conservative, 
@@ -24,10 +24,10 @@ runPC <- function(X, parentsOf, alpha, variableSelMat, setOptions, directed, ver
                verbose= verbose)
   pcmat <- as(pc.fit@graph, "matrix")
   if(directed) pcmat <- pcmat * (t(pcmat)==0)
-  
-  for (k in 1:length(parentsOf)){
-    result[[k]] <- which(as.logical(pcmat[, parentsOf[k]]))
-  }
-  
-  result
+    
+    for (k in 1:length(parentsOf)){
+        result[[k]] <- which(as.logical(pcmat[, parentsOf[k]]))
+    }
+    
+    result
 }
